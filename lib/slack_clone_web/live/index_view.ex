51c0a121a9cd4acf71457263f239b4946f6e75a2
Pedro Molina <pedro.ivo.molina@gmail.com>
2023-01-15 18:05:49 -0300

    feat: added current_room_id to be passed to Sidebar component and added handle_event function

--- lib/slack_clone_web/live/index_view.ex
+++ lib/slack_clone_web/live/index_view.ex
@@ -18,10 +18,15 @@ defmodule SlackCloneWeb.Live.IndexView do
     ~H"""
     <div class="w-full border shadow bg-white">
       <div class="flex">
-        <.live_component module={Components.Sidebar} id="sidebar" current_user={@current_user} />
+        <.live_component module={Components.Sidebar} id="sidebar" current_user={@current_user} current_room_id={@current_room_id} />
         <.live_component module={Components.Messages} id="messages" room_id={@current_room_id} />
       </div>
     </div>
     """
+  end
+
+  @impl true
+  def handle_event("enter-room", %{"id" => room_id} =  _event, socket) do
+    {:noreply, assign(socket, current_room_id: room_id)}
   end
 end
